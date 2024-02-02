9a771cdef43fe50ab776d34bdfb5c060c44dcc53
Parker Brandt <parker.brandt101@gmail.com>
2024-02-02 09:29:21 -0600

    Initial commit

---
+++ core/src/com/parkerbrandt/match/MTGMatch.java
@@ -0,0 +1,13 @@
+package com.parkerbrandt.match;
+
+public class MTGMatch {
+
+    /**
+     * Properties
+     */
+    private String player1;
+    private String player2;
+
+
+
+}
---
+++ core/src/com/parkerbrandt/server/Server.java
@@ -0,0 +1,4 @@
+package com.parkerbrandt.server;
+
+public class Server {
+}
