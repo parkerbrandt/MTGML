db60281236aea98d2f010dd9d340793ea3138846
Parker Brandt <parker.brandt101@gmail.com>
2024-02-02 10:03:22 -0600

    Begun implementing simple LibGDX example

--- core/src/com/parkerbrandt/game/MTGSim.java
+++ core/src/com/parkerbrandt/game/MTGSim.java
@@ -1,31 +1,86 @@
 package com.parkerbrandt.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MTGSim extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	/*
+	 * Properties
+	 */
+
+	// Assets
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
+	// Game Objects
+	private Rectangle bucket;
+
+
+	/*
+	 * Override Methods
+	 */
+
+
 	@Override
 	public void create () {
+		// Load images
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// Load sound effects and music
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		// Start playback of background music
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		// Create the camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		// Create the sprite batch to draw the 2D images
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		// Initialize our bucket
+		bucket = new Rectangle();
+		bucket.x = 800/2 - 64/2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		// Clear screen with a dark blue color
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		camera.update();
+
+		// Draw the bucket
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketImage, bucket.x, bucket.y);
 		batch.end();
+
+		// Allow for mouse interaction
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
--- desktop/src/com/parkerbrandt/game/desktop/DesktopLauncher.java
+++ desktop/src/com/parkerbrandt/game/desktop/DesktopLauncher.java
@@ -1,12 +1,19 @@
 package com.parkerbrandt.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.parkerbrandt.game.MTGSim;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MTGSim(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		// Set window parameters
+		config.setTitle("Learning Magic: The Gathering");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+
+		new Lwjgl3Application(new MTGSim(), config);
 	}
 }
