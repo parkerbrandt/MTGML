e2839b18104b3ccd38902c93fc6ae22469222e44
Parker Brandt <parker.brandt101@gmail.com>
2024-02-03 10:03:26 -0600

    Added some player info

--- core/src/com/parkerbrandt/match/Player.java
+++ core/src/com/parkerbrandt/match/Player.java
@@ -1,4 +1,34 @@
 package com.parkerbrandt.match;
 
+import com.badlogic.gdx.utils.Array;
+import io.magicthegathering.javasdk.resource.Card;
+
+
+/**
+ *
+ */
 public class Player {
+
+    /*
+     * Properties
+     */
+    private boolean isPlayer1;
+    private boolean useML = false;
+
+
+    /*
+     * Constructors
+     */
+    public Player(boolean isPlayer1, Array<Card> deck) {
+
+    }
+
+    public Player(boolean isPlayer1, String mlAlgo, Array<Card> deck) {
+
+    }
+
+
+    /*
+     * Methods
+     */
 }
